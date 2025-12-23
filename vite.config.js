868154a2b7ddc28a8d8454e00f6acfb37ee0ac31
Xinge Xu <xingexu1107@gmail.com>
2025-12-23 00:39:47 -0500

    Fix Vercel deploy: correct Vite base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/BarbershopWeb/" : "/",
+  base: process.env.VERCEL ? "/" : (mode === "production" ? "/BarbershopWeb/" : "/" ),
   plugins: [react()],
   server: {
     proxy: {
